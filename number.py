2c47ff10632f8e1347d26c83c782a6c1fbc8e609
Christopher Herrington <christopher.herrington@outlook.com>
2019-08-29 15:18:06 -0400

    add other practice files

---
+++ number.py
@@ -0,0 +1,5 @@
+value = int(input('Enter a number\n: '))
+
+if type(value) is int:
+    print(str(value) + ' * ' + str(value) + ' = ' + str(value * value))
+else: print(str(value) + "is not a number!")
